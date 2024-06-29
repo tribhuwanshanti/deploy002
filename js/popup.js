7d74dac8029da37d473169cb3848fa627395a0bf
tribhuwanshanti <shreetribhuwanshanti@gmail.com>
2024-06-29 12:23:34 +0545

    V-7.5

--- js/popup.js
+++ js/popup.js
@@ -1,5 +1,6 @@
 const popup = document.querySelector('.popup');
-        const x = document.querySelector('.popup-content div')
+        const x = document.querySelector('.popup p')
+        const popuppart = document.querySelector('.popup-content')
 
         window.addEventListener('load', () => {
             popup.classList.add('showPopup');
@@ -8,4 +9,7 @@ const popup = document.querySelector('.popup');
         x.addEventListener('click', () => {
             popup.classList.remove('showPopup');
             popup.childNodes[1].classList.remove('showPopup');
+        })
+        popuppart.addEventListener('click', () => {
+            window.location.href="https://forms.gle/x2iYb2PfuPuY367J7"
         })
